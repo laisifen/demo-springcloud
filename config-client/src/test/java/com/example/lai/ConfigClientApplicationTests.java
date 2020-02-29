45d2869b836efdeac8b3d81759630965ae23b596
lsf <1249030579@qq.com>
2020-02-29 20:15:01 +0800

    重命名

---
+++ config-client/src/test/java/com/example/lai/ConfigClientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.lai;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ConfigClientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
---
+++ server-turbine/src/main/java/com/example/serviceturbine/ServiceTurbineApplication.java
@@ -0,0 +1,17 @@
+package com.example.serviceturbine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
+import org.springframework.cloud.netflix.turbine.EnableTurbine;
+
+@EnableEurekaClient
+@EnableTurbine
+@SpringBootApplication
+public class ServiceTurbineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServiceTurbineApplication.class, args);
+	}
+
+}
